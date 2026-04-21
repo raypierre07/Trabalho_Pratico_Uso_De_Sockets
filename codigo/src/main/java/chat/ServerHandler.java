43939eb8228be8c6e7e7efc2d3ceb431c7cdcf8f
Eman134 <kaykeeman@gmail.com>
2026-04-21 14:10:40 -0300

    feat: Implementação da criação de servidor socket

--- codigo/src/main/java/chat/ServerHandler.java
+++ codigo/src/main/java/chat/ServerHandler.java
@@ -2,27 +2,68 @@ package chat;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServerHandler {
 
-    public void searchServer() {
-
-    }
+    private Map<Integer, ChatServerThread> activeServers = new HashMap<>();
+    private int nextServerId = 1;
 
     public String[] getServers() {
-        return null;
+        if (activeServers.isEmpty()) {
+            return null;
+        }
+        String[] servers = new String[activeServers.size()];
+        int i = 0;
+        for (Map.Entry<Integer, ChatServerThread> entry : activeServers.entrySet()) {
+            servers[i] = "ID: " + entry.getKey() + " | Porta: " + entry.getValue().getPort();
+            i++;
+        }
+        return servers;
     }
 
-    public void connectServer() {
-
+    public int createServer(Integer port) throws IOException {
+        ChatServerThread server = new ChatServerThread(port);
+        server.start();
+        int id = nextServerId++;
+        activeServers.put(id, server);
+        return id;
     }
 
-    public void disconnectServer() {
-
+    public int getPortById(int id) {
+        ChatServerThread server = activeServers.get(id);
+        return server != null ? server.getPort() : -1;
     }
 
-    public void createServer(Integer port) {
+    private static class ChatServerThread extends Thread {
+        private ServerSocket serverSocket;
+        private int port;
+        private boolean running = true;
 
+        public ChatServerThread(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(port);
+                while (running) {
+                    Socket clientSocket = serverSocket.accept();
+                    ChatConnection connection = new ChatConnection(clientSocket);
+                    connection.start();
+                }
+            } catch (IOException e) {
+                if (running) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public int getPort() {
+            return port;
+        }
     }
-
 }
